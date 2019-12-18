fda47c3c25b0962c84835719373a772e356e0851
yangxiaoning <1732702632@qq.com>
2019-12-18 16:54:20 +0800

    添加一个分号

---
+++ src/components/child/son.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul v-for="j in obj">
+      <li>{{j}}</li>
+    </ul>
+    <div>{{send}}</div>
+    <input @click="sendData" type="button" value="传值">
+    <slot></slot>
+    <div>{{gets}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'son',
+    props: ['send'],
+    data() {
+      return {
+        obj: ['children', 'sons', 'class'],
+        paren: '他是个吧',
+        gets: ""
+      }
+    },
+    methods: {
+      sendData() {
+        this.$emit('pas', this.paren)
+      }
+    },
+    mounted(){
+      console.log(this.$route.params.dataObj)
+    }
+  }
+</script>
+<style>
+  body {
+
+  }
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,49 @@
+<template>
+ <div>
+   <input v-for="j in arr" type="button" :value="j">
+   <son :send="sondata" @pas="getChild">
+    <h1>我是杨大宁slot的传值</h1>
+   </son>
+   <div>{{name}}</div>
+   <div :class="yangxiaoning">{{sondata}}</div>
+   <div>{{msh}}</div>
+   <button @click="ths">是个狗</button>
+ </div>
+</template>
+
+<script>
+  import son from './child/son';
+  import {mapGetters,mapActions} from  'vuex';
+  export default {
+    name: 'test',
+    components:{
+      son
+    },
+    data() {
+      return {
+        arr:['yangxiaoning','wanglinna','liyong'],
+        sondata:'他是个够',
+        name:'',
+        yangxiaoning:['divs']
+      }
+    },
+    computed:{
+      ...mapGetters(['msh'])
+    },
+   methods:{
+      getChild(data){
+        this.name=data
+      },
+     ...mapActions(['ths'])
+    },
+    mounted(){
+      console.log(this.$route.query.name)
+    }
+  }
+</script>
+
+<style scoped>
+  .divs{
+    font-size: 30px;
+  }
+</style>
