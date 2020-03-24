d805a241b1575702a84c5cd9acb5b50686bba67d
yangxiaoning <1732702632@qq.com>
2020-03-24 16:18:51 +0800

    添加一个constract

---
+++ src/components/constract.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>{{adds}}</div>
+    <son @pass="adds"></son>
+    <div v-html="msd"></div>
+  </div>
+
+</template>
+
+<script>
+  import son from './child/son';
+    export default {
+        data() {
+            return {
+              adds:'这是一个新增的界面',
+              msd:'<h1>我是</h1>'
+            }
+        },
+        components: {
+          son
+        }
+    }
+</script>
+<style>
+    body {
+        background-color: #ff0000;
+    }
+</style>
